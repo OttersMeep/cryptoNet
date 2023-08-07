9ba251975b46263fab620d04b63fbe930f9eb2fe
OttersMeep <107761109+OttersMeep@users.noreply.github.com>
2023-08-06 22:49:12 -0400

    Create

---
+++ index.js
@@ -0,0 +1,9 @@
+// Add Express
+const express = require("express");
+// Initialize Express
+const app = express();
+// Create GET request
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+module.exports = app;
