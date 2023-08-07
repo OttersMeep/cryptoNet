2a120d3856ca7d29125c9515a6d9f245a77b3922
OttersMeep <107761109+OttersMeep@users.noreply.github.com>
2023-08-06 22:59:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-// Add Express
+const crypto = require('crypto');
 const express = require("express");
 // Initialize Express
 const app = express();
 // Create GET request
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+app.get("/createkeypair/:KEY", (req, res) => {
+
+  // Generate key pair
+  var { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
+    modulusLength: 2048,
+    publicKeyEncoding: {
+      type: 'pkcs1',
+      format: 'pem'
+    },
+    privateKeyEncoding: {
+      type: 'pkcs1',
+      format: 'pem'
+    }
+  });
+  // Convert keys to plaintext
+  var privateKeyPlaintext = privateKey.toString('utf-8');
+  var publicKeyPlaintext = publicKey.toString('utf-8');
+
+  console.log('Private Key:');
+  console.log(privateKeyPlaintext);
+
+  console.log('Public Key:');
+  console.log(publicKeyPlaintext);
+
 });
 module.exports = app;
